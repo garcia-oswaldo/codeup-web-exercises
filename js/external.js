cf3fd6613d6066a020e09faf919c9a0638668d95
oswaldo garcia <garcia.oswaldo1000@gmail.com>
2021-05-27 15:51:52 -0500

    update functions.js.html

--- js/external.js
+++ js/external.js
@@ -2,12 +2,21 @@
 
 //... rest of javascript code
 
-console.log("hello world");
-console.log("hello polaris");
-console.log("hello students");
+//console.log("hello world");
+//console.log("hello polaris");
+//console.log("hello students");
 //alert("message goes here")
 // the following line will show the OK/CANCEL confirm dialog
 //var confirmed = confirm('Are you sure you want to do XYZ?');
 //console.log(confirmed); // will be either true or false
-var userInput = prompt('Please type something:');
-console.log('The user entered: ' + userInput);
+//var userInput = prompt('Please type something:');
+//console.log('The user entered: ' + userInput);
+alert("The price of movies is" + (3+5+1)*3);
+
+//Answer to problem 3 1
+alert("I am getting paid:" + (10*350)+(6*400)+(4*380));
+
+
+
+
+
