a976b310d05b4a4bafa479e3ccbfeff03b8fea99
oswaldo garcia <garcia.oswaldo1000@gmail.com>
2021-05-27 11:48:02 -0500

    this is practice

--- js/external.js
+++ js/external.js
@@ -1,1 +1,13 @@
-get
+"use strict";
+
+//... rest of javascript code
+
+console.log("hello world");
+console.log("hello polaris");
+console.log("hello students");
+//alert("message goes here")
+// the following line will show the OK/CANCEL confirm dialog
+//var confirmed = confirm('Are you sure you want to do XYZ?');
+//console.log(confirmed); // will be either true or false
+var userInput = prompt('Please type something:');
+console.log('The user entered: ' + userInput);
