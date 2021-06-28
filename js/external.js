c36d9aba96d7b69b138323c71911d38cc85f103b
oswaldo garcia <garcia.oswaldo1000@gmail.com>
2021-06-28 18:50:09 -0500

    updated incomplet external js

--- js/external.js
+++ js/external.js
@@ -2,7 +2,11 @@
 
 //... rest of javascript code
 
-//console.log("hello world");
+console.log("hello world");
+alert("welcome to my website.");
+var userInput = prompt('What is your favorite color?:');
+alert('Great ' + userInput + ' is my favorite color as well!' );
+console.log('Great ' + userInput +  'is my favorite color as well!' );
 //console.log("hello polaris");
 //console.log("hello students");
 //alert("message goes here")
@@ -11,10 +15,29 @@
 //console.log(confirmed); // will be either true or false
 //var userInput = prompt('Please type something:');
 //console.log('The user entered: ' + userInput);
-alert("The price of movies is" + (3+5+1)*3);
-
-//Answer to problem 3 1
-alert("I am getting paid:" + (10*350)+(6*400)+(4*380));
+// alert("The price of movies is" + (3+5+1)*3);
+//
+// //Answer to problem 3 1
+// alert("I am getting paid:" + (10*350)+(6*400)+(4*380));
+//
+//
+var schedule
+var class
+var enrolledClassess
+if(class < 99 && schedule != enrolledClasses){
+    return enroll;
+}
+//
+var promotion=20%
+var price;
+var purchaseItems;
+var nopromotion =0
+var purchasePrice= (promotion *price) + price;
+if(purchaseItems <2){
+    return nopromotion;
+} else  {
+    return purchasePrice;
+}
 
 
 
