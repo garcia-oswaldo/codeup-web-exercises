b6bf1921ce6e2855755601a0cf0401d042dadec8
oswaldo garcia <garcia.oswaldo1000@gmail.com>
2021-06-28 17:47:27 -0500

    update

--- js/while.js
+++ js/while.js
@@ -10,8 +10,24 @@ while (value < 65537) {
       console log before incrementing
       increment loop*/
 
-var num1=5;
-while(num1<200){
-  console.log("this is the number: " + num1);
-  num1= num1+num1;
-}
+// var num1=5;
+// while(num1<200){
+//   console.log("this is the number: " + num1);
+//   num1= num1+num1;
+// }
+
+
+let totalSold = 0;
+
+do {
+  let conesBought = Math.ceil(Math.random() * (5 - 1) + 1);
+
+  if ((totalSold + conesBought) > totalCanSell) {
+    console.log("I'm sorry, I can't sell " + conesBought);
+    continue;
+  }
+  console.log("My customer bought" + conesBought);
+  totalSold = totalSold + conesBought;
+  console.log("I've sold this many " + totalSold);
+
+} while (totalSold < totalCanSell);
