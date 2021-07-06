8a544a210ffb6eaa33ae04a59fde3d544eb2052e
oswaldo garcia <garcia.oswaldo1000@gmail.com>
2021-07-06 09:30:49 -0500

    morning work

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -27,3 +27,33 @@ I was not able to do the problems. I need practice on this.
 
 
 // TODO: Add validation to function above, if the argument pass is not a string it should return false.
+
+
+// July 6th 2021
+// TODO: Create a function named capitalizeAllNames that accepts an array of strings and returns the array
+//  with all of its elements capitalized.
+// Example:  capitalizeAllNames(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["BOB","SETH","TOFU","TOYOTA"]
+
+function capitalizeAllNames (str)
+{
+
+    var arrayUpper = str.toUpperCase();
+    return upper;
+}
+
+
+
+// TODO: Create a function named capitalizeFirstLetter that accepts an array of strings and
+//  returns the array with all of the first letters in
+//  each string being capitalized while having the rest of the
+//  string lowercase look at the example below for reference.
+// Example:  capitalizeFirstLetter(["bob","Seth","TOFU","Toyota"]) ---- returns ---->
+// ["Bob","Seth","Tofu","Toyota"]
+
+function capitalizeFirstLetter(str){
+   let str= str.toLowerCase().split("")
+    for(let i=0; i<str.length;i++){
+        str[i]=str[i][0].toUpperCase() + str[i].slice(1)
+    }
+    return str.join("")
+}
