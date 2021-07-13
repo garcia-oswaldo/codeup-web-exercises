c4d883b9195c8197af04644d48fdeac3da38f763
oswaldo garcia <garcia.oswaldo1000@gmail.com>
2021-07-13 12:27:45 -0500

    Upsate

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,11 +1,19 @@
 "use strict"
 
 $(document).ready(function() {
-    alert("hello");
-    $('dd').addClass('invisible')
 
+    alert("hello");
+    // adds class invisible to dd;
+    $('dd').addClass('invisible')
+    //css addeded invisible
+    //add clickevent to toggle invisible
     $('#push').click(function(event) {
         event.preventDefault();
+        //toggle method invisible
         $('dd').toggleClass('invisible');
     });
+    $('dt').click(function(){
+        $(this).css('background-color', 'yellow')
 });
+});
+
